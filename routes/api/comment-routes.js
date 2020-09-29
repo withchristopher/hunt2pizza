bb588efafd8ec0892da62d66defbba6d0b957d9b
Chris Maxwell <chmaxwel@icloud.com>
2020-09-29 17:04:10 +0200

    correction

---
+++ routes/api/comment-routes.js
@@ -0,0 +1,6 @@
+const { removeComment } = require('../../controllers/comment-controller');
+
+const router = require('express').Router();
+const { addComment, removeComment } = require('../../controllers/comment-controller');
+
+module.exports = router;
