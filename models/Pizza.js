63739e300b7cd8627c26c4e27cd77ce06c0500bf
Chris Maxwell <chmaxwel@icloud.com>
2020-09-30 13:05:45 +0200

    Update Pizza.js

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const moment = require('moment');
 
 const PizzaSchema = new Schema({
     pizzaName: {
@@ -9,7 +10,8 @@ const PizzaSchema = new Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
     },
     size: {
         type: String,
@@ -25,6 +27,7 @@ const PizzaSchema = new Schema({
 }, {
     toJSON: {
         virtuals: true,
+        getters: true
     },
     id: false
 });
